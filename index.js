8c0a54c9f834505b39b7c2d0e9a53267087ab3ad
Xotic750 <xotic750@gmail.com>
2016-01-21 09:32:39 +0100

    Cleanup

--- index.js
+++ index.js
@@ -40,7 +40,7 @@
  * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
  * behave as closely as possible to ECMAScript 6 (Harmony).
  *
- * @version 1.0.6
+ * @version 1.0.7
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
@@ -52,26 +52,21 @@
 /*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-  es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:3,
-  maxstatements:9, maxcomplexity:4 */
+  es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:2,
+  maxstatements:8, maxcomplexity:4 */
 
 /*global module */
 
 ;(function () {
   'use strict';
 
-  var isObjectLike = require('is-object-like-x');
-  var getSize;
+  var isObjectLike, getSize;
 
   if (typeof Map === 'function') {
     try {
-      getSize = Object.getOwnPropertyDescriptor(
-        Object.getPrototypeOf(new Map()),
-        'size'
-      ).get;
-      if (typeof getSize.call(new Map()) !== 'number') {
-        throw 'not a number';
-      }
+      getSize = Object.getOwnPropertyDescriptor(Map.prototype, 'size').get;
+      getSize = typeof getSize.call(new Map()) === 'number' && getSize;
+      isObjectLike = require('is-object-like-x');
     } catch (ignore) {
       getSize = null;
     }
