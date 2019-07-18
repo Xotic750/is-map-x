d5c2a699f0b5ad4931ce109dc3fb86f8faeb7e4a
Graham Fairweather <xotic750@gmail.com>
2019-07-19 00:49:54 +0200

    :bookmark: v2.0.3
    Auto build

--- dist/is-map-x.js
+++ dist/is-map-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-18T00:29:02.254Z",
+  "date": "2019-07-18T22:49:47.313Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 Map.",
   "file": "is-map-x.js",
   "hash": "43cebf6a2acc8f9d1350",
   "license": "MIT",
-  "version": "2.0.2"
+  "version": "2.0.3"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
