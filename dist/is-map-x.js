214086e043cbc6f5290f7298519fe9e51a48e6df
Graham Fairweather <xotic750@gmail.com>
2019-08-06 03:08:39 +0200

    :bookmark: v2.0.15
    Coveralls

--- dist/is-map-x.js
+++ dist/is-map-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-05T18:55:51.990Z",
+  "date": "2019-08-06T01:08:33.375Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 Map.",
   "file": "is-map-x.js",
