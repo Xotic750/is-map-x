88e71e69ca90020e66a94aec8d58e10b87210be6
Graham Fairweather <xotic750@gmail.com>
2019-08-08 00:44:27 +0200

    Lodash security fix

--- dist/is-map-x.js
+++ dist/is-map-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-07T01:48:36.969Z",
+  "date": "2019-08-07T22:44:21.340Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 Map.",
   "file": "is-map-x.js",
