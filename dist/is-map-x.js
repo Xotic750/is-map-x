8f18c03304bec7254921a734bbfe8a9260a2cf29
Graham Fairweather <xotic750@gmail.com>
2019-08-28 19:55:54 +0200

    :bookmark: v2.1.2
    Security update

--- dist/is-map-x.js
+++ dist/is-map-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-28T15:53:32.879Z",
+  "date": "2019-08-28T17:55:48.581Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 Map.",
   "file": "is-map-x.js",
   "hash": "cd8463fb173311ba2182",
   "license": "MIT",
-  "version": "2.1.1"
+  "version": "2.1.2"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
